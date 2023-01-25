f1602d107dc33b1ce3b226396b8ae28d028e191c
Sarah Bunker <ChemEnCoder@gmail.com>
2023-01-25 16:58:48 -0700

    can you see a blank string

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ const App = () => {
 
   const getEndpoints = async () => {
     const endpoints = await requestService.getAllEndpoints()
-    return JSON.stringify(endpoints)
+    return ""
   }
 
   return (
