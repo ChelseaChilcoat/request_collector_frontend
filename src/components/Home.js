753720490aabe4f69487e187764e28f88f74dee0
ChelseaSaunders <c.c.saunders@protonmail.com>
2023-01-26 18:18:35 -0800

    added ryan reynolds

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,21 @@
 import EndpointForm from './endpointform'
 import ListEndpoints from './ListEndpoints'
+import './styles.css'
 
 const Home = ({ createEndpoint, removeEndpoint, endpointPathArray, updateEndpointArray }) => {
   return (
     <div>
-      <h1>Welcome to the Dumpster of Disappointment!</h1>
+      <h1>Welcome to the Dumpster of Disappointment*!</h1>
       <EndpointForm createEndpoint={createEndpoint} updateEndpointArray={updateEndpointArray} />
       <ListEndpoints endpointPathArray={endpointPathArray} removeEndpoint={removeEndpoint} />
+      <div>
+        <p>*Don't be too disappointed, though...there's always Ryan Reynolds...
+          <img src={require("./ryan_reynolds.jpeg")} alt="Ryan Reynolds" />
+        </p>
+      </div>
   </div>
   )
+
 };
 
 export default Home;
